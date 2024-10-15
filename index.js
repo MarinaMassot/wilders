aa5cd05816aa03945b0cd3a5333cb0af8398e654
MARINA MASSOT <marina.massot1@gmail.com>
2024-10-15 11:02:59 +0200

    resolve exercise

---
+++ index.js
@@ -0,0 +1,6 @@
+import chalk from 'chalk';
+
+console.log(chalk.cyanBright('Claire'));
+console.log(chalk.bgMagenta('Bryan'));
+console.log(chalk.yellowBright('Aude'));
+console.log(chalk.bgGreen('Emilie'));
